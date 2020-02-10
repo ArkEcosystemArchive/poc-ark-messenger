662be4b8bca8d0f98f7d8248346197c1f4dde43a
Lemii <lemiidelegate@gmail.com>
2020-02-10 10:34:57 +0100

    refactor: optimize RegEx

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,5 +1,5 @@
 export const truncateChannel = (channel: string, pad: number = 9) => {
-  const regex = /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{34}$/;
+  const regex = /^[0-9A-Za-z]{34}$/;
 
   if (channel.match(regex) || channel.length >= 28) {
     const start = channel.substr(0, pad);
